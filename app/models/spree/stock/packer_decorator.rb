ea3c52a338df0b40f179335196604fbb3381306a
David Ang <davidang09@gmail.com>
2014-12-12 04:19:30 +0800

    updated Packer#product_assembly_package method so it compatible with the spree 2.4 way of adding items to a package

--- app/models/spree/stock/packer_decorator.rb
+++ app/models/spree/stock/packer_decorator.rb
@@ -31,11 +31,8 @@ module Spree
                 units = variant_inventory_units.clone # this could be wrong to count the quantity of assembled product in order    
 
                 on_hand, backordered = stock_location.fill_status(part, units.count * product.count_of(part))
-                package.add units.slice!(0, on_hand), :on_hand if on_hand > 0
-                package.add units.slice!(0, backordered), :backordered if backordered > 0
-
-                #package.add line_item, on_hand, :on_hand, part if on_hand > 0
-                #package.add line_item, backordered, :backordered, part if backordered > 0
+                package.add_multiple units.slice!(0, on_hand), :on_hand if on_hand > 0
+                package.add_multiple units.slice!(0, backordered), :backordered if backordered > 0
               else
                 package.add units.slice!(0, on_hand), :on_hand
               end
@@ -52,34 +49,6 @@ module Spree
           end
         end
         package        
-
-
-        # package = Package.new(stock_location, order)
-        # order.line_items.each do |line_item|
-        #   product = line_item.product
-        #   if product.assembly?
-        #     line_item.parts.each do |part|
-        #       if part.should_track_inventory?
-        #         next unless stock_location.stock_item(part)
-
-        #         on_hand, backordered = stock_location.fill_status(part, line_item.quantity * product.count_of(part))
-        #         package.add line_item, on_hand, :on_hand, part if on_hand > 0
-        #         package.add line_item, backordered, :backordered, part if backordered > 0
-        #       else
-        #         package.add line_item, line_item.quantity * product.count_of(part), :on_hand, part
-        #       end
-        #     end
-        #   elsif line_item.should_track_inventory?
-        #     next unless stock_location.stock_item(line_item.variant)
-
-        #     on_hand, backordered = stock_location.fill_status(line_item.variant, line_item.quantity)
-        #     package.add line_item, on_hand, :on_hand, line_item.variant if on_hand > 0
-        #     package.add line_item, backordered, :backordered, line_item.variant if backordered > 0
-        #   else
-        #     package.add line_item, line_item.quantity, :on_hand, line_item.variant
-        #   end
-        # end
-        # package
       end
     end
   end
